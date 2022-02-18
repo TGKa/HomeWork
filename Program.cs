9d921a56cd9d5f22a0700e4ef1476465f2c5ae2f
TGKa <99993120+TGKa@users.noreply.github.com>
2022-02-18 23:48:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 100;
+            short age = 5;
+            ushort weightInGrams = 1050;
+            int coins = 10;
+            ulong Bricks = 50000;
+            float volumeInLiters = 10.53f;
+            double money = 100.35;
+            char symbol = '#';
+            string nameCity = "Las Vegas";
+            bool fullTank = false;
+        }
+    }
+}
